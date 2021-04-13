5503fc3bb09b6f4de9dcbfa19c66bc4c53b01597
maainul <mainul080@yahoo.com>
2021-04-13 16:53:35 +0600

    created

---
+++ src/main/java/com/restcrud/springbootmysqljpahibernaterestfulcrudapiexample/SpringBootMySqlJpaHibernateRestfulCrudApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.restcrud.springbootmysqljpahibernaterestfulcrudapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqlJpaHibernateRestfulCrudApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMySqlJpaHibernateRestfulCrudApiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restcrud/springbootmysqljpahibernaterestfulcrudapiexample/SpringBootMySqlJpaHibernateRestfulCrudApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restcrud.springbootmysqljpahibernaterestfulcrudapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlJpaHibernateRestfulCrudApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
